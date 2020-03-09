b7570c9b1c06026a6098de12c45a953df3bb357d
Emilee Mitchell <emcakes@gmail.com>
2020-03-09 17:24:22 -0500

    Add project files.

---
+++ LegoMiniFigures/Astronaut.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMiniFigures
+{
+    class Astronaut
+    {
+        // public proerty
+        public Location Location { get; set; }
+
+        // Name == "Space Person"..... this is the expression and it is detereming what the value of this property is
+        //Expression Bodied Property (always readonly)
+        public bool SuitedUp => Name != "Space Person";
+
+
+        //   public string Name { get; } ... because we don't have "set;" it is only setable inside the constructor
+        // Read-Only Property
+        public string Name { get; }
+
+        // we can set it once but only within the file it is created.
+        // public property with a private setter
+        public string Job { get; private set; }
+        public int OxygenLevel { private get; set; }
+
+        // ctor tab tab - to create my own contructor // the only time you're going to write a constructor is if you want to use something when that class gets instantiated.
+        public Astronaut(string name, string job)
+        {
+            Name = name;
+            Job = job;
+
+        }
+        // what behaviors do we want to add?
+        public void Promote()
+        {
+            Job = "Commander of Janitors";
+        }
+
+        public void DoYourJob()
+        {
+            Console.WriteLine($"{Name} is doing all their {Job} duties...");
+        }
+
+
+    }
+}
---
+++ LegoMiniFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LegoMiniFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates a new instance of the Astronaut class and we ae storing a reference to where that instance lives inside this variable. What part of the Astronaut class? the CONSTRUCTOR
+            var astronaut = new Astronaut("Space Person", "Janitor");
+            var astronaut2 = new Astronaut("Space Lady", "Janitor");
+
+            astronaut.DoYourJob();
+
+            var casper = new Ghost("Casper", DateTime.Today)
+            {
+                Friendly = true
+            };
+
+            casper.Spook();
+            casper.Haunt("Whipstaff Manor");
+            casper.Spook();
+
+
+            var fatso = new Ghost("Fatso", new DateTime(1924, 12, 1))
+            {
+                Friendly = false
+            };
+
+
+            fatso.Spook();
+            fatso.Haunt("Whipstaff Manor");
+            fatso.Spook();
+
+        }
+    }
+}
+
